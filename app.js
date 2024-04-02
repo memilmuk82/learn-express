b61ffe162eb02dd1a7677ebe20288fcf31f7f362
JinseonLee <memilmuk82@gmail.com>
2024-04-03 07:53:12 +0900

    익스프레스 배워봅시다

--- app.js
+++ app.js
@@ -12,16 +12,16 @@ app.set('port', process.env.PORT || 3000);
 // app.set(키, 값)에 데이터를 저장, 데이터를 app.get(키)로 가져올 수 있음.
 
 // 루트 경로('/')로 GET 요청이 오면 index.html 파일을 클라이언트에게 전송합니다.
-
 app.get('/', (req, res) => {
-    // app.get(주소, 라우터) ->  주소에 대한 GET요청이 올때 어떤 동작을 할지 적는 부분
-    // req -> 요청에 대한 정보가 들어 있는 객체
-    // res -> 응답에 대한 정보가 들어있는 객체    
     res.sendFile(path.join(__dirname, '/index.html'));
-    // res.sendFile() -> 클라이언트에게 파일을 전송하는 메서드
-    // path.join(__dirname, '/index.html') -> 서버의 디렉토리 경로와 'index.html' 파일의 상대 경로를 결합하여 전체 파일 경로를 생성
-    // __dirname -> 현재 스크립트가 위치한 디렉토리를 나타내는 Node.js 전역 변수
 });
+// app.get(주소, 라우터) ->  주소에 대한 GET요청이 올때 어떤 동작을 할지 적는 부분
+// req -> 요청에 대한 정보가 들어 있는 객체
+// res -> 응답에 대한 정보가 들어있는 객체   
+// res.sendFile() -> 클라이언트에게 파일을 전송하는 메서드
+// path.join(__dirname, '/index.html') -> 서버의 디렉토리 경로와 'index.html' 파일의 상대 경로를 결합하여 전체 파일 경로를 생성
+// __dirname -> 현재 스크립트가 위치한 디렉토리를 나타내는 Node.js 전역 변수
+
 
 // 앱이 설정한 포트에서 대기 상태로 시작됩니다. 
 // 서버가 시작되면 해당 포트에서 대기 중 메시지를 로그에 출력합니다.
